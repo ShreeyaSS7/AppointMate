022d50a7c198b3cb196edd56b524091e6df1a61e
Shriya Rane <shriyarane10@gmail.com>
2025-09-14 16:03:09 +0530

    im gonna cry

--- BookAppt/form_appt.js
+++ BookAppt/form_appt.js
@@ -3,7 +3,19 @@ document.querySelector('.forms').addEventListener('submit', function(e) {
 
     alert('Form submitted!');
 
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const appointments = JSON.parse(localStorage.getItem("appointments")) || [];
 
+  // Check if slot is free globally
+  const conflict = appointments.some(a =>
+    a.doc === newAppt.doc && a.date === newAppt.date && a.time === newAppt.time
+  );
+  if (conflict) {
+    alert("This time slot is already booked!");
+    return;
+  }
+
+  const doctor=localStorage.getItem("doctor");
   const formData = {
     name: document.getElementById('name').value,
     age: document.getElementById('age').value,
@@ -13,16 +25,23 @@ document.querySelector('.forms').addEventListener('submit', function(e) {
     date: document.getElementById('appointment-date').value,
     time: document.getElementById('appointment-time').value,
     reason: document.getElementById('reason').value,
-    visitType: document.getElementById('first').value
+    visitType: document.getElementById('first').value,
+    doctor: doctor,
   };
 
+  const newAppt={
+    date: document.getElementById('appointment-date').value,
+    time: document.getElementById('appointment-time').value,
+    doctor: doctor,
+    email: document.getElementById('email').value,
+  }
   console.log('Form data:', formData);
   //alert('Data collected: ' + JSON.stringify(formData));
   
 
   
   sessionStorage.setItem('appointmentData', JSON.stringify(formData));
-  
+
   console.log('Stored data:', sessionStorage.getItem('appointmentData'));
 
 
